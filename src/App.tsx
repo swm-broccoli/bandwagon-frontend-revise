2b2144278b82adb2d7f5dd8bf4d2ad14def7c95f
witch-factory <soakdma37@gmail.com>
2022-07-27 18:12:52 +0900

    initial setting commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-amber-700 text-4xl underline'>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
