ea50586c956bd3ee12acca03ff153d07a6be430a
witch-factory <soakdma37@gmail.com>
2022-08-01 00:37:15 +0900

    로그인하러 가기 버튼 추가

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -188,6 +188,12 @@ function SignUpPage() {
           <span className='text-error'>*</span> 는 필수 입력 사항
         </p>
         <SignUpForm />
+        <div className='text-accent text-sm w-60 md:w-80 mt-5 flex justify-between'>
+          <span>이미 계정이 있나요?</span>
+          <Link to='/login' className='text-primary'>
+            로그인하러 가기
+          </Link>
+        </div>
       </div>
       <GlobalFooter />
     </div>
