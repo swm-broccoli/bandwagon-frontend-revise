e96f1cde9e7bae4145163f49141143fbdfc036c3
witch-factory <soakdma37@gmail.com>
2022-07-31 17:15:44 +0900

    선택 인풋 추가

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -5,20 +5,27 @@ function TextInput({
   placeholder,
   password = false,
   required = false,
-}: {
+  essential = false,
+  ...props
+}: // 회원가입 등 입력 항목에서 필수 항목인지
+{
   label: string;
   placeholder: string;
   password?: boolean;
   required?: boolean;
+  essential?: boolean;
 }) {
   return (
-    <div className='flex flex-col my-2'>
-      <label className='text-accent mb-2'>{label}</label>
+    <div className='flex flex-col mt-5'>
+      <label className='text-accent mb-2 text-sm'>
+        {label} {essential ? <span className='text-error'>*</span> : null}
+      </label>
       <input
         required={required}
         type={password ? 'password' : 'text'}
         placeholder={placeholder}
         className='input input-bordered w-60 md:w-80 focus:outline-none focus:border-primary invalid:border-error text-accent'
+        {...props}
       />
     </div>
   );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GlobalNavBar from '../../components/NavBar';
+import TextInput from '../../components/TextInput';
+
+function DateInput({ label }: { label: string }) {
+  return (
+    <div className='flex flex-col mt-5'>
+      <label className='text-accent mb-2 text-sm'>{label}</label>
+      <input
+        type='date'
+        placeholder='1997-01-29'
+        className='input input-bordered w-60 md:w-80 focus:outline-none focus:border-primary invalid:border-error text-accent'
+      />
+    </div>
+  );
+}
+
+function SelectionInput({
+  label,
+  selectionList,
+}: {
+  label: string;
+  selectionList: string[];
+}) {
+  const [curSelection, setCurSelection] = useState(selectionList[0]);
+
+  return (
+    <div className='flex flex-col mt-5 w-60 md:w-80'>
+      <label className='text-accent mb-2 text-sm'>{label}</label>
+      <div className='grid grid-flow-col space-x-3'>
+        {selectionList.map((selection) => {
+          return (
+            <button
+              key={selection}
+              className={`btn text-base-300 font-normal ${
+                curSelection === selection
+                  ? 'bg-success border-primary text-primary hover:bg-[#bbf7d0] hover:border-primary'
+                  : 'bg-base-100 border-base-300 text-base-300 hover:bg-success hover:border-primary'
+              }`}
+              onClick={() => setCurSelection(selection)}
+            >
+              {selection}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function SignUpForm() {
+  const genderSelection = ['남자', '여자'];
+
+  return (
+    <form className='mt-5'>
+      <TextInput label='이름' placeholder='김성현' required essential />
+      <TextInput label='닉네임' placeholder='asdf1234' required essential />
+      <TextInput
+        label='이메일'
+        placeholder='ksh1234@gmail.com'
+        required
+        essential
+      />
+      <TextInput
+        label='비밀번호'
+        placeholder='1234'
+        password
+        required
+        essential
+      />
+      <TextInput
+        label='비밀번호 확인'
+        placeholder='1234'
+        password
+        required
+        essential
+      />
+      <DateInput label='생년월일' />
+      <SelectionInput label='성별' selectionList={genderSelection} />
+    </form>
+  );
+}
 
 function SignUpPage() {
-  return <div>회원가입 페이지입니다.</div>;
+  return (
+    <div>
+      <GlobalNavBar />
+      <div className='flex flex-col items-center mt-10'>
+        <h1 className='text-3xl font-bold'>Sign Up</h1>
+        <p className='mt-3 text-neutral'>
+          <span className='text-error'>*</span> 는 필수 입력 사항
+        </p>
+        <SignUpForm />
+      </div>
+    </div>
+  );
 }
 
 export default SignUpPage;
