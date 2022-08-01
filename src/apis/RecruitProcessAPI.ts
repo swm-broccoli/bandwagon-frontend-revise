9eda142c76766b9fee8561bd647a3f6101549f6f
nabivi <nerdring54@gmail.com>
2022-08-01 13:07:04 +0900

    style: API 관련 기본 파일 추가

---
+++ src/apis/RecruitProcessAPI.ts
@@ -0,0 +1,13 @@
+// 지원 가능 조건, 지원 양식 관련 API
+
+import {request} from './request';
+
+interface RecruitProcessApiType {
+
+}
+
+const RecruitProcessAPI: RecruitProcessApiType = {
+
+}
+
+export default RecruitProcessAPI;
