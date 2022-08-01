ef73a9ff27e4d957a0eb71f10ed06b5f42f0ff65
witch-factory <soakdma37@gmail.com>
2022-08-01 21:16:29 +0900

    myPageMenu 항목 배열 파일로 분리

--- src/components/MyPageMenu.tsx
+++ src/components/MyPageMenu.tsx
@@ -1,5 +1,64 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import MyInfoIcon from '../assets/ico_my1@2x.png';
+import MyInfoActiveIcon from '../assets/ico_my1_on@2x.png';
+import BandInfoIcon from '../assets/live_music.png';
+import BandInfoActiveIcon from '../assets/live_music_on.png';
+import AccountInfoIcon from '../assets/ico_my2@2x.png';
+import AccountInfoActiveIcon from '../assets/ico_my2_on@2x.png';
+import PasswordChangeIcon from '../assets/ico_my3@2x.png';
+import PasswordChangeActiveIcon from '../assets/ico_my3_on@2x.png';
+import MyPostIcon from '../assets/ico_my4@2x.png';
+import MyPostActiveIcon from '../assets/ico_my4_on@2x.png';
+import MyCommentIcon from '../assets/ico_my5@2x.png';
+import MyCommentActiveIcon from '../assets/ico_my5_on@2x.png';
+import MyLikeIcon from '../assets/ico_my6@2x.png';
+import MyLikeActiveIcon from '../assets/ico_my6_on@2x.png';
+
+export const myPageMenuItems = [
+  {
+    label: '내 정보',
+    to: '/profile/user',
+    icon: MyInfoIcon,
+    activeIcon: MyInfoActiveIcon,
+  },
+  {
+    label: '밴드 정보',
+    to: '/profile/band',
+    icon: BandInfoIcon,
+    activeIcon: BandInfoActiveIcon,
+  },
+  {
+    label: '계정 정보',
+    to: '/edit/account',
+    icon: AccountInfoIcon,
+    activeIcon: AccountInfoActiveIcon,
+  },
+  {
+    label: '비밀번호 변경',
+    to: '/edit/password',
+    icon: PasswordChangeIcon,
+    activeIcon: PasswordChangeActiveIcon,
+  },
+  {
+    label: '내 글',
+    to: '/profile/mypost',
+    icon: MyPostIcon,
+    activeIcon: MyPostActiveIcon,
+  },
+  {
+    label: '내 댓글',
+    to: '/profile/mycomment',
+    icon: MyCommentIcon,
+    activeIcon: MyCommentActiveIcon,
+  },
+  {
+    label: '찜한 글',
+    to: '/profile/mylike',
+    icon: MyLikeIcon,
+    activeIcon: MyLikeActiveIcon,
+  },
+];
 
 interface MyPageMenuItemProps {
   label: string;
--- src/pages/EditPassword/index.tsx
+++ src/pages/EditPassword/index.tsx
@@ -1,72 +1,15 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import GlobalNavBar from '../../components/NavBar';
 import { Link, useLocation } from 'react-router-dom';
-import MyInfoIcon from '../../assets/ico_my1@2x.png';
-import MyInfoActiveIcon from '../../assets/ico_my1_on@2x.png';
-import BandInfoIcon from '../../assets/live_music.png';
-import BandInfoActiveIcon from '../../assets/live_music_on.png';
-import AccountInfoIcon from '../../assets/ico_my2@2x.png';
-import AccountInfoActiveIcon from '../../assets/ico_my2_on@2x.png';
-import PasswordChangeIcon from '../../assets/ico_my3@2x.png';
-import PasswordChangeActiveIcon from '../../assets/ico_my3_on@2x.png';
-import MyPostIcon from '../../assets/ico_my4@2x.png';
-import MyPostActiveIcon from '../../assets/ico_my4_on@2x.png';
-import MyCommentIcon from '../../assets/ico_my5@2x.png';
-import MyCommentActiveIcon from '../../assets/ico_my5_on@2x.png';
-import MyLikeIcon from '../../assets/ico_my6@2x.png';
-import MyLikeActiveIcon from '../../assets/ico_my6_on@2x.png';
-import MyPageMenu from '../../components/MyPageMenu';
-
-const myPageMenuItems = [
-  {
-    label: '내 정보',
-    to: '/profile/user',
-    icon: MyInfoIcon,
-    activeIcon: MyInfoActiveIcon,
-  },
-  {
-    label: '밴드 정보',
-    to: '/profile/band',
-    icon: BandInfoIcon,
-    activeIcon: BandInfoActiveIcon,
-  },
-  {
-    label: '계정 정보',
-    to: '/edit/account',
-    icon: AccountInfoIcon,
-    activeIcon: AccountInfoActiveIcon,
-  },
-  {
-    label: '비밀번호 변경',
-    to: '/edit/password',
-    icon: PasswordChangeIcon,
-    activeIcon: PasswordChangeActiveIcon,
-  },
-  {
-    label: '내 글',
-    to: '/profile/mypost',
-    icon: MyPostIcon,
-    activeIcon: MyPostActiveIcon,
-  },
-  {
-    label: '내 댓글',
-    to: '/profile/mycomment',
-    icon: MyCommentIcon,
-    activeIcon: MyCommentActiveIcon,
-  },
-  {
-    label: '찜한 글',
-    to: '/profile/mylike',
-    icon: MyLikeIcon,
-    activeIcon: MyLikeActiveIcon,
-  },
-];
+import MyPageMenu, { myPageMenuItems } from '../../components/MyPageMenu';
 
 function EditInput({
   label,
   password = false,
+  name,
 }: {
   label: string;
+  name?: string;
   password?: boolean;
 }) {
   return (
@@ -78,6 +21,7 @@ function EditInput({
         <input
           type={password ? 'password' : 'text'}
           placeholder='Type here'
+          name={name}
           className='input input-bordered w-3/5 max-w-xs focus:outline-none focus:border-primary text-accent'
         />
       </div>
@@ -98,10 +42,14 @@ function PasswordChangeForm({ label }: { label: string }) {
         </div>
 
         <div className='mt-6 row-start-2 col-span-full'>
-          <EditInput label='아이디' />
-          <EditInput label='현재 비밀번호' password />
-          <EditInput label='새 비밀번호' password />
-          <EditInput label='새 비밀번호 확인' password />
+          <EditInput label='아이디' name='id' />
+          <EditInput label='현재 비밀번호' password name='curPassword' />
+          <EditInput label='새 비밀번호' password name='newPassword' />
+          <EditInput
+            label='새 비밀번호 확인'
+            password
+            name='newPasswordConfirm'
+          />
         </div>
       </form>
     </div>
