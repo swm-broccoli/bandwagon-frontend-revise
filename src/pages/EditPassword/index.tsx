c9984b88fad1494a27afc10dd9600a0cf12fbb83
witch-factory <soakdma37@gmail.com>
2022-08-01 17:01:26 +0900

    비밀번호 변경 페이지 레이아웃

--- src/pages/EditPassword/index.tsx
+++ src/pages/EditPassword/index.tsx
@@ -62,14 +62,63 @@ const myPageMenuItems = [
   },
 ];
 
+function EditInput({
+  label,
+  password = false,
+}: {
+  label: string;
+  password?: boolean;
+}) {
+  return (
+    <>
+      <div className='form-control w-full flex flex-row justify-start py-2'>
+        <label className='label w-1/5 py-0'>
+          <span className='label-text text-accent'>{label}</span>
+        </label>
+        <input
+          type={password ? 'password' : 'text'}
+          placeholder='Type here'
+          className='input input-bordered w-3/5 max-w-xs focus:outline-none focus:border-primary text-accent'
+        />
+      </div>
+      <div className='divider m-0 w-5/6' />
+    </>
+  );
+}
+
+function PasswordChangeForm({ label }: { label: string }) {
+  return (
+    <div className='grid auto-rows-min'>
+      <form>
+        <div className='flex flex-row justify-between'>
+          <h1 className='text-bold text-2xl font-bold'>{label}</h1>
+          <button type='submit' className='btn btn-primary h-10'>
+            비밀번호 변경
+          </button>
+        </div>
+
+        <div className='mt-6 row-start-2 col-span-full'>
+          <EditInput label='아이디' />
+          <EditInput label='현재 비밀번호' password />
+          <EditInput label='새 비밀번호' password />
+          <EditInput label='새 비밀번호 확인' password />
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function PasswordEditPage() {
   return (
     <div>
       <GlobalNavBar />
       <div className='grid grid-cols-6'>
-        <div className='col-start-2 col-end-6 grid grid-cols-5'>
-          <div className='col-span-full md:col-start-1 md:col-span-1 min-w-[150px]'>
+        <div className='col-start-2 col-end-6 flex flex-col md:flex-row'>
+          <div className='w-full md:w-1/5 md:col-span-1 min-w-[150px] mt-10'>
             <MyPageMenu menuList={myPageMenuItems} />
+          </div>
+          <div className='pl-5 pt-5 md:pt-12 w-full'>
+            <PasswordChangeForm label={`비밀번호 변경`} />
           </div>
         </div>
       </div>
