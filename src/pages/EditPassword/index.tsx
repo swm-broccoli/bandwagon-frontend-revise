16379b9e267633d6f7415616b16fbd501320dda2
witch-factory <soakdma37@gmail.com>
2022-08-01 15:07:25 +0900

    컴포넌트 분리, 반응형으로 메뉴 항목 간격 조정

--- src/pages/EditPassword/index.tsx
+++ src/pages/EditPassword/index.tsx
@@ -15,115 +15,52 @@ import MyCommentIcon from '../../assets/ico_my5@2x.png';
 import MyCommentActiveIcon from '../../assets/ico_my5_on@2x.png';
 import MyLikeIcon from '../../assets/ico_my6@2x.png';
 import MyLikeActiveIcon from '../../assets/ico_my6_on@2x.png';
+import MyPageMenu from '../../components/MyPageMenu';
 
-function MyPageMenuItem({
-  label,
-  to,
-  icon,
-  activeIcon,
-  active,
-}: {
-  label: string;
-  to: string;
-  icon: string;
-  activeIcon: string;
-  active: boolean;
-}) {
-  return (
-    <li
-      className={`flex flex-row mx-3 items-center rounded-lg ${
-        active
-          ? 'bg-success hover:bg-[#bbf7d0]'
-          : 'bg-base-100 hover:bg-base-200'
-      }`}
-    >
-      <Link
-        to={to}
-        className={`pl-2 w-full ${
-          active
-            ? 'bg-success hover:bg-[#bbf7d0]'
-            : 'bg-base-100 hover:bg-base-200'
-        }`}
-      >
-        <img
-          src={active ? activeIcon : icon}
-          alt={label}
-          className='w-5 h-5 rounded-none'
-        />
-        <span
-          className={`${active ? 'text-primary' : 'text-neutral'} text-center`}
-        >
-          {label}
-        </span>
-      </Link>
-    </li>
-  );
-}
-
-function MyPageMenu() {
-  const menuItems = [
-    {
-      label: '내 정보',
-      to: '/profile/user',
-      icon: MyInfoIcon,
-      activeIcon: MyInfoActiveIcon,
-      active: true,
-    },
-    {
-      label: '밴드 정보',
-      to: '/profile/band',
-      icon: BandInfoIcon,
-      activeIcon: BandInfoActiveIcon,
-    },
-    {
-      label: '계정 정보',
-      to: '/edit/account',
-      icon: AccountInfoIcon,
-      activeIcon: AccountInfoActiveIcon,
-    },
-    {
-      label: '비밀번호 변경',
-      to: '/edit/password',
-      icon: PasswordChangeIcon,
-      activeIcon: PasswordChangeActiveIcon,
-    },
-    {
-      label: '내 글',
-      to: '/profile/mypost',
-      icon: MyPostIcon,
-      activeIcon: MyPostActiveIcon,
-    },
-    {
-      label: '내 댓글',
-      to: '/profile/mycomment',
-      icon: MyCommentIcon,
-      activeIcon: MyCommentActiveIcon,
-    },
-    {
-      label: '찜한 글',
-      to: '/profile/mylike',
-      icon: MyLikeIcon,
-      activeIcon: MyLikeActiveIcon,
-    },
-  ];
-
-  let location = useLocation();
-
-  return (
-    <ul className='menu rounded-box border border-base-200 p-1'>
-      {menuItems.map((item, index) => (
-        <MyPageMenuItem
-          key={index}
-          label={item.label}
-          to={item.to}
-          icon={item.icon}
-          activeIcon={item.activeIcon}
-          active={item.to === location.pathname}
-        />
-      ))}
-    </ul>
-  );
-}
+const myPageMenuItems = [
+  {
+    label: '내 정보',
+    to: '/profile/user',
+    icon: MyInfoIcon,
+    activeIcon: MyInfoActiveIcon,
+  },
+  {
+    label: '밴드 정보',
+    to: '/profile/band',
+    icon: BandInfoIcon,
+    activeIcon: BandInfoActiveIcon,
+  },
+  {
+    label: '계정 정보',
+    to: '/edit/account',
+    icon: AccountInfoIcon,
+    activeIcon: AccountInfoActiveIcon,
+  },
+  {
+    label: '비밀번호 변경',
+    to: '/edit/password',
+    icon: PasswordChangeIcon,
+    activeIcon: PasswordChangeActiveIcon,
+  },
+  {
+    label: '내 글',
+    to: '/profile/mypost',
+    icon: MyPostIcon,
+    activeIcon: MyPostActiveIcon,
+  },
+  {
+    label: '내 댓글',
+    to: '/profile/mycomment',
+    icon: MyCommentIcon,
+    activeIcon: MyCommentActiveIcon,
+  },
+  {
+    label: '찜한 글',
+    to: '/profile/mylike',
+    icon: MyLikeIcon,
+    activeIcon: MyLikeActiveIcon,
+  },
+];
 
 function PasswordEditPage() {
   return (
@@ -132,7 +69,7 @@ function PasswordEditPage() {
       <div className='grid grid-cols-6'>
         <div className='col-start-2 col-end-6 grid grid-cols-5'>
           <div className='col-span-full md:col-start-1 md:col-span-1 min-w-[150px]'>
-            <MyPageMenu />
+            <MyPageMenu menuList={myPageMenuItems} />
           </div>
         </div>
       </div>
