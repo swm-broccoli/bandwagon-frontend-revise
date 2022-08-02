a867915579230acaa5c6dafbcf2affed69ca632d
witch-factory <soakdma37@gmail.com>
2022-08-02 15:46:34 +0900

    모달 고유 아이디를 랜덤 생성해서 포지션-지역 선택이 분리될 수 있게 함

--- src/components/ProfileAddModal.tsx
+++ src/components/ProfileAddModal.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { v4 } from 'uuid';
+
 function ProfileAddModal({
   label,
   addSelected,
@@ -10,28 +13,31 @@ function ProfileAddModal({
   // 라벨, 취소 버튼, 추가 버튼이 포함된 모달을 여는 '+추가' 버튼을 렌더링한다.
   // 추가 버튼을 누르면 선택된 옵션을 선택된 옵션 배열에 추가한다.
   // 라벨과 취소/추가 버튼 사이에는 옵션 드롭박스를 렌더링한다.
+
+  let modalID = v4();
+
   return (
     <>
       <label
-        htmlFor='add-modal'
+        htmlFor={modalID}
         className='btn btn-primary btn-sm h-8 w-14 mr-1 p-0 modal-button'
       >
         +추가
       </label>
-      <input type='checkbox' id='add-modal' className='modal-toggle' />
+      <input type='checkbox' id={modalID} className='modal-toggle' />
       <div className='modal'>
         <div className='modal-box relative flex flex-col items-center'>
           <h3 className='text-lg mb-6'>{label}</h3>
           {children}
           <div className='flex justify-center mt-4 w-1/2'>
             <label
-              htmlFor='add-modal'
+              htmlFor={modalID}
               className='btn bg-base-[#c5c5c5] w-1/2 text-base-100 border-none mx-2'
             >
               취소
             </label>
             <label
-              htmlFor='add-modal'
+              htmlFor={modalID}
               onClick={addSelected}
               className='btn btn-primary w-1/2 mx-2'
             >
--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -1,14 +1,165 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MyPageTemplate from '../../components/MyPageTemplate';
 import ProfileReadOnlyTextField from '../../components/ProfileReadOnlyTextField';
 import ProfileSelectField from '../../components/ProfileSelectField';
+import { AreaType } from '../../types/types';
+import areaOptions from '../../assets/options/areaOptions';
+import ProfileAddModal from '../../components/ProfileAddModal';
+
+function AreaFieldItem({
+  area,
+  editing,
+  deleteArea,
+}: {
+  area: AreaType;
+  editing: boolean;
+  deleteArea: () => void;
+}) {
+  if (editing) {
+    return (
+      <div className='mr-2'>
+        {`${area.city} ${area.district}`}
+        <button onClick={deleteArea}>X</button>
+      </div>
+    );
+  } else {
+    return <div className='mr-2'>{`${area.city} ${area.district}`}</div>;
+  }
+}
+
+function AreaFieldAddButton({
+  label,
+  editing,
+  areas,
+  setAreas,
+  options,
+}: {
+  label: string;
+  editing: boolean;
+  areas: AreaType[];
+  setAreas: (areas: AreaType[]) => void;
+  options: AreaType[];
+}) {
+  const [curAreaOption, setCurAreaOption] = useState<AreaType>(options[0]);
+
+  const cityOptions = options
+    .map((area) => area.city)
+    .filter((city, index, self) => self.indexOf(city) === index);
+
+  if (!editing) {
+    return null;
+  } else {
+    return (
+      <ProfileAddModal
+        label={`${label} 추가`}
+        addSelected={() => {
+          setAreas(areas.concat(curAreaOption));
+        }}
+      >
+        <div className='flex flex-row justify-center'>
+          <select
+            value={curAreaOption.city}
+            onChange={(e) =>
+              setCurAreaOption(
+                options.find((option) => option.city === e.target.value) ||
+                  options[0],
+              )
+            }
+            className='select select-bordered w-full max-w-xs'
+          >
+            {cityOptions.map((city, index) => (
+              <option key={index} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+          <select
+            value={curAreaOption.district}
+            onChange={(e) => {
+              setCurAreaOption(
+                options.find(
+                  (option) =>
+                    option.city === curAreaOption.city &&
+                    option.district === e.target.value,
+                ) || options[0],
+              );
+            }}
+            className='select select-bordered w-full max-w-xs'
+          >
+            {options
+              .filter((area) => {
+                return area.city === curAreaOption.city;
+              })
+              .map((area, index) => (
+                <option key={index} value={area.district}>
+                  {area.district}
+                </option>
+              ))}
+          </select>
+        </div>
+      </ProfileAddModal>
+    );
+  }
+}
+
+function AreaField({
+  label,
+  areas,
+  setAreas,
+  options,
+}: {
+  label: string;
+  areas: AreaType[];
+  setAreas: (areas: AreaType[]) => void;
+  options: AreaType[];
+}) {
+  const [editing, setEditing] = useState(false);
+
+  return (
+    <>
+      <div className='form-control h-10 w-full flex flex-row justify-start items-center my-2'>
+        <label className='label w-1/5 py-0'>
+          <span className='label-text text-accent'>{label}</span>
+        </label>
+        <div className='flex flex-row items-center h-10 w-3/5 mr-2 text-accent'>
+          {areas.map((area, index) => (
+            <AreaFieldItem
+              key={index}
+              area={area}
+              editing={editing}
+              deleteArea={() => {
+                setAreas(areas.filter((_, i) => i !== index));
+              }}
+            />
+          ))}
+        </div>
+        <AreaFieldAddButton
+          label={label}
+          editing={editing}
+          areas={areas}
+          setAreas={setAreas}
+          options={options}
+        />
+        <button
+          onClick={() => {
+            setEditing((prev) => !prev);
+          }}
+          className='btn btn-sm bg-base-100 hover:bg-base-200 border-base-200 text-accent h-8 w-14 p-0'
+        >
+          {editing ? '완료' : '수정'}
+        </button>
+      </div>
+      <div className='divider m-0' />
+    </>
+  );
+}
 
 function UserProfile() {
   const [curUserProfile, setCurUserProfile] = useState({
     name: '김성현',
     birthday: '1996-01-01',
     positions: ['일렉기타'],
-    areas: [],
+    areas: [{ id: 2, city: '서울', district: '중구' }],
     genres: [],
     description: '',
     userPerformances: [],
@@ -42,6 +193,17 @@ function UserProfile() {
                 positions: newPosition,
               });
             }}
+          />
+          <AreaField
+            label='지역'
+            areas={curUserProfile.areas}
+            setAreas={(newAreas) => {
+              setCurUserProfile({
+                ...curUserProfile,
+                areas: newAreas,
+              });
+            }}
+            options={areaOptions}
           />
         </div>
       </div>
