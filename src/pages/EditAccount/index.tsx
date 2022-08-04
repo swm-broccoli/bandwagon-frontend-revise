de9ccc69bb12e68d5b96e9d3a87a34408a357606
witch-factory <soakdma37@gmail.com>
2022-08-04 19:46:26 +0900

    성별 제외한 계정정보 수정 적용

--- src/pages/EditAccount/index.tsx
+++ src/pages/EditAccount/index.tsx
@@ -1,18 +1,67 @@
 import MyPageTemplate from '../../components/MyPageTemplate';
 import EditPageInput from '../../components/EditPageInput';
 import UserAccountAPI from '../../apis/UserAccountAPI';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+interface UserAccountFormType {
+  name: string;
+  nickname: string;
+  email: string;
+  gender: boolean;
+  birthday: string;
+}
+
+function parseAccountEditForm(form: UserAccountFormType) {
+  return {
+    ...form,
+    birthday: form.birthday.split('T')[0],
+  };
+}
 
 function AccountEditForm({ label }: { label: string }) {
-  useEffect(() => {
-    UserAccountAPI.getUserAccountInfo().then((res) => {
-      console.log(res.data);
-    });
+  const [userAccountInfo, setUserAccountInfo] = useState<UserAccountFormType>({
+    name: '',
+    nickname: '',
+    email: '',
+    gender: false,
+    birthday: '',
   });
+
+  const onAccountEditSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    UserAccountAPI.updateUserAccountInfo(userAccountInfo)
+      .then((res) => {
+        alert('수정 성공');
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    UserAccountAPI.getUserAccountInfo()
+      .then((res) => {
+        setUserAccountInfo(parseAccountEditForm(res.data));
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const onEditInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(userAccountInfo);
+    setUserAccountInfo({
+      ...userAccountInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={onAccountEditSubmit}>
         <div className='flex flex-row justify-between'>
           <h1 className='text-bold text-2xl font-bold'>{label}</h1>
           <button type='submit' className='btn btn-primary h-10'>
@@ -21,13 +70,30 @@ function AccountEditForm({ label }: { label: string }) {
         </div>
 
         <div className='mt-6 row-start-2 col-span-full'>
-          <EditPageInput label='이름' name='name' />
-          <EditPageInput label='생년월일' type='date' name='birthday' />
-          <EditPageInput label='이메일' name='email' />
           <EditPageInput
-            label='현재 비밀번호'
-            type='password'
-            name='password'
+            label='이메일'
+            name='email'
+            value={userAccountInfo.email}
+            onChange={onEditInfo}
+          />
+          <EditPageInput
+            label='이름'
+            name='name'
+            value={userAccountInfo.name}
+            onChange={onEditInfo}
+          />
+          <EditPageInput
+            label='닉네임'
+            name='nickname'
+            value={userAccountInfo.nickname}
+            onChange={onEditInfo}
+          />
+          <EditPageInput
+            label='생년월일'
+            type='date'
+            name='birthday'
+            value={userAccountInfo.birthday}
+            onChange={onEditInfo}
           />
         </div>
       </form>
