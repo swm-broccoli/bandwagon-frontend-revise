452a33878c5ae15bba55702c75426811b51f7d38
witch-factory <soakdma37@gmail.com>
2022-08-07 16:15:38 +0900

    사용자 프로필 수정 api(연주기록제외)구현

--- src/apis/UserProfileAPI.ts
+++ src/apis/UserProfileAPI.ts
@@ -4,12 +4,52 @@ import { request } from './request';
 
 interface UserProfileApiType {
   getUserProfileInfo: () => Promise<AxiosResponse>;
+  addUserPosition: (positionID: number) => Promise<AxiosResponse>;
+  deleteUserPosition: (positionID: number) => Promise<AxiosResponse>;
+  addUserGenre: (genreID: number) => Promise<AxiosResponse>;
+  deleteUserGenre: (genreID: number) => Promise<AxiosResponse>;
+  addUserArea: (areaID: number) => Promise<AxiosResponse>;
+  deleteUserArea: (areaID: number) => Promise<AxiosResponse>;
+  setUserDescription: (description: string) => Promise<AxiosResponse>;
 }
 
 const UserProfileAPI: UserProfileApiType = {
   getUserProfileInfo: () => {
     const userID = localStorage.getItem('userID');
     return request.get(`/api/users/${userID}/mypage`);
+  },
+  addUserPosition: (positionID) => {
+    const userID = localStorage.getItem('userID');
+    return request.post(
+      `/api/users/${userID}/positions/${positionID}`,
+      positionID,
+    );
+  },
+  deleteUserPosition: (positionID) => {
+    const userID = localStorage.getItem('userID');
+    return request.delete(`/api/users/${userID}/positions/${positionID}`);
+  },
+  addUserGenre: (genreID) => {
+    const userID = localStorage.getItem('userID');
+    return request.post(`/api/users/${userID}/genres/${genreID}`, genreID);
+  },
+  deleteUserGenre: (genreID) => {
+    const userID = localStorage.getItem('userID');
+    return request.delete(`/api/users/${userID}/genres/${genreID}`);
+  },
+  addUserArea: (areaID) => {
+    const userID = localStorage.getItem('userID');
+    return request.post(`/api/users/${userID}/areas/${areaID}`, areaID);
+  },
+  deleteUserArea: (areaID) => {
+    const userID = localStorage.getItem('userID');
+    return request.delete(`/api/users/${userID}/areas/${areaID}`);
+  },
+  setUserDescription: (description) => {
+    const userID = localStorage.getItem('userID');
+    return request.put(`/api/users/${userID}/description`, {
+      description: description,
+    });
   },
 };
 
