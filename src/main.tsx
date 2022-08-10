6cfb5d88641b6ce32485eb5edfb50f1f92d0ee84
nabivi <nerdring54@gmail.com>
2022-08-10 17:54:15 +0900

    fix: useLoginStore 상태에 따라 유지되도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { useLoginStore } from './stores/LoginStore';
+
+useLoginStore.getState().checkLoggedIn();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
