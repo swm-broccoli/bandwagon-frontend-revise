4743888e986bb8fcf3ac0f53be28f0c2ee8ffdee
witch-factory <soakdma37@gmail.com>
2022-08-10 18:55:58 +0900

    기본 밴드 프로필 수정

--- src/pages/BandProfile/index.tsx
+++ src/pages/BandProfile/index.tsx
@@ -8,8 +8,7 @@ import {
   BandProfileAlbum,
   ProfileTextField,
 } from './styles';
-import initialBandProfile from './initialBandProfile';
-import { BandProfileType, PictureType } from '../../types/types';
+import { BandProfileType } from '../../types/types';
 import areaOptions from '../../assets/options/areaOptions';
 import weekdayOptions from '../../assets/options/weekdayOptions';
 import genreOptions from '../../assets/options/genreOptions';
@@ -25,6 +24,8 @@ import {
   updateBandGenres,
   updateBandDescription,
 } from './bandProfileUpdate';
+import EmptyBandProfile from './EmptyBandProfile';
+import { vacantBandProfile } from './initialBandProfile';
 
 function parseBandProfile(bandProfile: BandProfileType) {
   return {
@@ -33,95 +34,14 @@ function parseBandProfile(bandProfile: BandProfileType) {
   };
 }
 
-function BandMakingForm() {
-  const [bandName, setBandName] = useState<string>('');
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    //폼은 제출시 자동 새로고침된다
-    console.log(bandName, '밴드로 제출됨');
-    BandProfileAPI.createBand(bandName)
-      .then((res) => {
-        if (res.status === 200) {
-          setBandName('');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className='flex flex-col'>
-      <label className='text-lg'>만들 밴드 이름</label>
-      <input
-        value={bandName}
-        onChange={(e) => {
-          setBandName(e.target.value);
-        }}
-        className='input input-bordered mt-3'
-      />
-      <button type='submit' className='btn btn-primary btn-sm h-10 mt-3'>
-        만들기
-      </button>
-    </form>
-  );
-}
-
-function EmptyBandProfile({ emptyBandPicture }: { emptyBandPicture: string }) {
-  const [bandMaking, setBandMaking] = useState<boolean>(false);
-
-  return (
-    <section>
-      <h1 className='text-bold text-2xl font-bold'>밴드 정보</h1>
-      <div className='grid grid-flow-row justify-center'>
-        <img src={emptyBandPicture} alt='밴드가 없을 때 사진' />
-        가입한 밴드가 존재하지 않습니다!
-        <button
-          onClick={() => {
-            setBandMaking((prev) => !prev);
-          }}
-          className='btn btn-sm bg-base-100 hover:bg-base-200 h-8 '
-        >
-          새 밴드 만들기
-        </button>
-        {bandMaking ? <BandMakingForm /> : null}
-      </div>
-    </section>
-  );
-}
-
 function BandProfile() {
   const [profileEditing, setProfileEditing] = useState<boolean>(false);
 
-  const [curBandProfile, setCurBandProfile] = useState<BandProfileType>({
-    id: -1,
-    avatarUrl: '',
-    name: '',
-    bandMembers: [],
-    areas: [],
-    genres: [],
-    days: [],
-    description: '',
-    bandPractices: [],
-    bandGigs: [],
-    bandPhotos: [],
-    isReaderFrontman: false,
-  });
+  const [curBandProfile, setCurBandProfile] =
+    useState<BandProfileType>(vacantBandProfile);
 
-  const [serverBandProfile, setServerBandProfile] = useState<BandProfileType>({
-    id: -1,
-    avatarUrl: '',
-    name: '',
-    bandMembers: [],
-    areas: [],
-    genres: [],
-    days: [],
-    description: '',
-    bandPractices: [],
-    bandGigs: [],
-    bandPhotos: [],
-    isReaderFrontman: false,
-  });
+  const [serverBandProfile, setServerBandProfile] =
+    useState<BandProfileType>(vacantBandProfile);
 
   useEffect(() => {
     BandProfileAPI.getBandProfileInfo()
