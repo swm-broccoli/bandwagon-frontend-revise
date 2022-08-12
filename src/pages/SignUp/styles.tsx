70fa65eebdfe889e24287321e73d6082095eeb55
witch-factory <soakdma37@gmail.com>
2022-08-12 22:54:11 +0900

    성별 선택란 디자인 변경

--- src/pages/SignUp/styles.tsx
+++ src/pages/SignUp/styles.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import SubmitButton from '../../components/SubmitButton';
 import TextInput from '../../components/TextInput';
 import CheckBox from '../../components/CheckBox';
 import DateInput from '../../components/DateInput';
 import SelectionInput from '../../components/SelectionInput';
 import AuthAPI from '../../apis/AuthAPI';
 import { useNavigate } from 'react-router-dom';
+
+interface CheckBoxItemType {
+  text: string;
+  checked: boolean;
+}
 
 function TermAgreeBox({
   label,
@@ -15,15 +19,27 @@ function TermAgreeBox({
   setAgreed: (newAgreed: boolean) => void;
 }) {
   //약관 동의 박스
-  const [checkedList, setCheckedList] = useState<boolean[]>([
-    false,
-    false,
-    false,
-    false,
+  const [checkedList, setCheckedList] = useState<CheckBoxItemType[]>([
+    {
+      text: '만 14세 이상입니다.',
+      checked: false,
+    },
+    {
+      text: '서비스 이용약관에 동의합니다.',
+      checked: false,
+    },
+    {
+      text: '개인정보 수집 및 이용에 동의합니다.',
+      checked: false,
+    },
+    {
+      text: '위치정보 이용약관에 동의합니다.',
+      checked: false,
+    },
   ]);
 
   useEffect(() => {
-    setAgreed(checkedList.every((checked) => checked));
+    setAgreed(checkedList.every((checked) => checked.checked === true));
   }, [checkedList]);
 
   return (
@@ -33,54 +49,42 @@ function TermAgreeBox({
         <div className='mt-2'>
           <CheckBox
             text='약관 전체 동의'
-            checked={checkedList.every((checked) => checked === true)}
+            checked={checkedList.every((checked) => checked.checked === true)}
             onClick={() => {
-              if (checkedList.every((checked) => checked === true)) {
+              if (checkedList.every((checked) => checked.checked === true)) {
                 //만약 모두 true
-                setCheckedList([false, false, false, false]);
+                setCheckedList(
+                  checkedList.map((checked) => ({
+                    ...checked,
+                    checked: false,
+                  })),
+                );
               } else {
-                setCheckedList([true, true, true, true]);
+                setCheckedList(
+                  checkedList.map((checked) => ({
+                    ...checked,
+                    checked: true,
+                  })),
+                );
               }
             }}
           />
         </div>
         <div className='divider m-0' />
-        <CheckBox
-          text='만 14세 이상입니다.'
-          checked={checkedList[0]}
-          onClick={() => {
-            setCheckedList((prev) => {
-              return prev.map((_, i) => (i === 0 ? !prev[i] : prev[i]));
-            });
-          }}
-        />
-        <CheckBox
-          text='이용약관 동의'
-          checked={checkedList[1]}
-          onClick={() => {
-            setCheckedList((prev) => {
-              return prev.map((_, i) => (i === 1 ? !prev[i] : prev[i]));
-            });
-          }}
-        />
-        <CheckBox
-          text='개인정보 수집 및 이용 동의'
-          checked={checkedList[2]}
-          onClick={() => {
-            setCheckedList((prev) => {
-              return prev.map((_, i) => (i === 2 ? !prev[i] : prev[i]));
-            });
-          }}
-        />
-        <CheckBox
-          text='마케팅 정보 수신 동의'
-          checked={checkedList[3]}
-          onClick={() => {
-            setCheckedList((prev) => {
-              return prev.map((_, i) => (i === 3 ? !prev[i] : prev[i]));
-            });
-          }}
-        />
+        {checkedList.map((checked, index) => (
+          <CheckBox
+            key={index}
+            text={checked.text}
+            checked={checked.checked}
+            onClick={() => {
+              setCheckedList(
+                checkedList.map((item, i) =>
+                  i === index ? { ...item, checked: !item.checked } : item,
+                ),
+              );
+            }}
+          />
+        ))}
       </div>
     </div>
   );
@@ -233,7 +237,12 @@ function SignUpForm() {
         selections={genderSelection}
       />
       <TermAgreeBox label='약관 동의' setAgreed={setTermAgreed} />
-      <SubmitButton label='가입하기' />
+      <button
+        type='submit'
+        className='btn btn-primary text-base-100 rounded-lg w-60 md:w-80 mt-7'
+      >
+        가입하기
+      </button>
     </form>
   );
 }
