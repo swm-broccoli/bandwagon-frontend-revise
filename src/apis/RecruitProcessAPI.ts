dd6ed535a34665536b956a79e58e2fd753b604ff
nabivi <nerdring54@gmail.com>
2022-08-16 16:32:46 +0900

    feat: 지원 조건 확인 API 메소드 추가

--- src/apis/RecruitProcessAPI.ts
+++ src/apis/RecruitProcessAPI.ts
@@ -1,13 +1,18 @@
 // 지원 가능 조건, 지원 양식 관련 API
 
+import { AxiosResponse } from 'axios';
 import {request} from './request';
 
 interface RecruitProcessApiType {
 
+  checkPrequisites: (postId: string) => Promise<AxiosResponse>
 }
 
 const RecruitProcessAPI: RecruitProcessApiType = {
 
+  checkPrequisites: (postId) => {
+    return request.get('/api/band/post/' + postId + '/prerequisites/check')
+  }
 }
 
 export default RecruitProcessAPI;
