a3cb71328cfe310065e981574014dd0d33bd612c
witch-factory <soakdma37@gmail.com>
2022-08-18 11:17:12 +0900

    jspdf html 모듈을 써서 pdf 제작하게 수정 - 폰트깨짐

--- src/pages/Portfolio/makePDF.ts
+++ src/pages/Portfolio/makePDF.ts
@@ -1,19 +1,24 @@
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
+import ReactDOM from 'react-dom';
 
 const makePDF = (elements: HTMLElement) => {
-  html2canvas(elements).then((canvas) => {
-    const imgData = canvas.toDataURL('image/png');
+  /*html2canvas(elements).then((canvas) => {
+    const imgData = canvas.toDataURL();
     const pdf = new jsPDF();
     pdf.addImage(
       imgData,
       'PNG',
       0,
       0,
-      elements.clientWidth,
-      elements.clientHeight,
+      pdf.internal.pageSize.getWidth(),
+      pdf.internal.pageSize.getHeight(),
     );
-    pdf.save('portfolio.pdf');
+    pdf.save(`portfolio-${Date.now()}.pdf`);
+  });*/
+  const pdf = new jsPDF('p', 'pt', 'a4');
+  pdf.html(elements, { margin: 10, html2canvas: { scale: 1.0 } }).then(() => {
+    pdf.save(`portfolio-${Date.now()}.pdf`);
   });
 };
 
