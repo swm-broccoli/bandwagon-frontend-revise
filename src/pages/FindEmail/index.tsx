be337b55f22bb95ff6ba38a7fd0e92eb78b3223f
witch-factory <soakdma37@gmail.com>
2022-08-18 14:31:03 +0900

    이메일 찾기 api 적용

--- src/pages/FindEmail/index.tsx
+++ src/pages/FindEmail/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import FindAccountAPI from '../../apis/FindAccountAPI';
 import DateInput from '../../components/DateInput';
 import GlobalFooter from '../../components/Footer';
 import GlobalNavBar from '../../components/NavBar';
@@ -12,36 +13,56 @@ function getTodayDate() {
 function FindEmailForm() {
   const [name, setName] = useState('');
   const [birthday, setBirthday] = useState(getTodayDate());
+  const [emailList, setEmailList] = useState([]);
 
   const onFindEmailSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(name, birthday);
+    FindAccountAPI.findEmail({ name: name, birthday: birthday })
+      .then((res) => {
+        console.log(res.data.emails);
+        setEmailList((prev) => res.data.emails);
+        console.log(emailList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-
-  return (
-    <form
-      onSubmit={onFindEmailSubmit}
-      className='flex flex-col items-center w-full min-w-[200px] mt-10'
-    >
-      <TextInput label='이름' value={name} setValue={setName} required />
-      <DateInput
-        label='생일'
-        value={birthday}
-        setValue={setBirthday}
-        required
-      />
-      <button
-        type='submit'
-        className={`btn rounded-lg w-60 md:w-80 mt-7 ${
-          name === '' && birthday === getTodayDate()
-            ? 'disabled text-base-100'
-            : 'btn-primary text-base-100'
-        }`}
+  if (emailList.length > 0) {
+    return (
+      <div>
+        <div className='text-xl mt-10'>가입하신 아이디는 다음과 같습니다.</div>
+        {emailList.map((email) => (
+          <span>{email}</span>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <form
+        onSubmit={onFindEmailSubmit}
+        className='flex flex-col items-center w-full min-w-[200px] mt-10'
       >
-        아이디 찾기
-      </button>
-    </form>
-  );
+        <TextInput label='이름' value={name} setValue={setName} required />
+        <DateInput
+          label='생일'
+          value={birthday}
+          setValue={setBirthday}
+          required
+        />
+        <button
+          type='submit'
+          className={`btn rounded-lg w-60 md:w-80 mt-7 ${
+            name === '' && birthday === getTodayDate()
+              ? 'disabled text-base-100'
+              : 'btn-primary text-base-100'
+          }`}
+        >
+          아이디 찾기
+        </button>
+      </form>
+    );
+  }
 }
 
 function FindEmailPage() {
