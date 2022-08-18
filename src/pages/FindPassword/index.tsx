6f2d916fb2f0b019c915f5a405e98cd701e4a981
witch-factory <soakdma37@gmail.com>
2022-08-18 15:14:59 +0900

    비밀번호 찾기 폼 파일 분리

--- src/pages/FindPassword/index.tsx
+++ src/pages/FindPassword/index.tsx
@@ -3,54 +3,8 @@ import FindAccountAPI from '../../apis/FindAccountAPI';
 import GlobalFooter from '../../components/Footer';
 import GlobalNavBar from '../../components/NavBar';
 import TextInput from '../../components/TextInput';
+import FindPasswordForm from './FindPasswordForm';
 import { useNavigate } from 'react-router-dom';
-
-function FindPasswordForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-
-  const navigate = useNavigate();
-
-  const onFindPasswordSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(name, email);
-
-    FindAccountAPI.findPassword({ name: name, email: email })
-      .then((res) => {
-        console.log(res);
-        if (res.status === 200) {
-          alert(
-            `입력하신 이메일 ${email}로 임시 비밀번호를 발송했습니다. 발송된 메일을 확인하고 로그인해 주세요.`,
-          );
-          navigate('/');
-        }
-      })
-      .catch((err) => {
-        alert(err.response.data.errorMessage);
-        navigate('/');
-      });
-  };
-
-  return (
-    <form
-      onSubmit={onFindPasswordSubmit}
-      className='flex flex-col items-center w-full min-w-[200px] mt-10'
-    >
-      <TextInput label='이름' value={name} setValue={setName} required />
-      <TextInput label='이메일' value={email} setValue={setEmail} required />
-      <button
-        type='submit'
-        className={`btn rounded-lg w-60 md:w-80 mt-7 ${
-          name === '' && email === ''
-            ? 'disabled text-base-100'
-            : 'btn-primary text-base-100'
-        }`}
-      >
-        비밀번호 찾기
-      </button>
-    </form>
-  );
-}
 
 function FindPasswordPage() {
   return (
