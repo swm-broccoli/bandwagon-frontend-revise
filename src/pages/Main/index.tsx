df1d9339924c82885c1c8feb3e4d7de2c56ab217
witch-factory <soakdma37@gmail.com>
2022-08-22 16:45:19 +0900

    추천 구인글 레이아웃 완성

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,10 +5,66 @@ import { bandPortfolioBrief } from './tempTodayPortfolio';
 import { TodayPortfolio } from './TodaysPortfolio';
 import GlobalFooter from '../../components/Footer';
 
-function RecommendedRecruitments() {
+interface RecommendedRecruitmentItemType {
+  image: string;
+  region: string;
+  title: string;
+}
+
+const tempRecommendedRecruitments: RecommendedRecruitmentItemType[] = [
+  {
+    image: 'https://picsum.photos/200',
+    region: '서울',
+    title: '서울 마포구 밴드 기타 모집합니다.',
+  },
+  {
+    image: 'https://picsum.photos/200',
+    region: '인천',
+    title: '인천 부평 밴드 여보컬 모집합니다.',
+  },
+  {
+    image: 'https://picsum.photos/200',
+    region: '서울',
+    title: '일산 백석 직밴 건반 모십니다.',
+  },
+];
+
+function RecommendedRecruitmentItem({
+  recruitment,
+}: {
+  recruitment: RecommendedRecruitmentItemType;
+}) {
   return (
-    <section>
-      <div>추천 구인 구직글</div>
+    <div className='grid grid-flow-row'>
+      <img
+        className='w-36 h-36 rounded-lg object-fill'
+        src={recruitment.image}
+      />
+      <span className='badge badge-secondary badge-outline'>
+        {recruitment.region}
+      </span>
+      <p>{recruitment.title}</p>
+    </div>
+  );
+}
+
+function RecommendedRecruitments({
+  recruitments,
+}: {
+  recruitments: RecommendedRecruitmentItemType[];
+}) {
+  return (
+    <section className='bg-[#f4f9f9] h-60 grid grid-cols-6'>
+      <div className='col-start-2'>
+        <h1 className='text-2xl font-bold'>추천 구인글</h1>
+        <h2 className='text-neutral'>R E C O M M E N D</h2>
+      </div>
+      {recruitments.map((recruitment) => (
+        <RecommendedRecruitmentItem
+          key={recruitment.title}
+          recruitment={recruitment}
+        />
+      ))}
     </section>
   );
 }
@@ -17,6 +73,7 @@ function MainPage() {
   return (
     <main>
       <GlobalNavBar />
+      <RecommendedRecruitments recruitments={tempRecommendedRecruitments} />
       <TodayPortfolio todayPortfolios={bandPortfolioBrief} />
       <div className='text-3xl text-teal-500'>메인 페이지입니다.</div>
       <div className='text-2xl text-teal-700'>페이지들의 링크는 아래에</div>
