1bcfb2c99d940c679a7821dca94f883309aeb3bc
witch-factory <soakdma37@gmail.com>
2022-08-25 16:27:29 +0900

    밴드명 수정 필드 너비 수정

--- src/pages/BandProfile/styles.tsx
+++ src/pages/BandProfile/styles.tsx
@@ -68,19 +68,19 @@ export function ProfileTextField({
   return (
     <>
       <div className='form-control h-10 w-full flex flex-row justify-start items-center my-2'>
-        <label className='label w-1/5 py-0'>
+        <label className='label w-1/4 min-w-[60px] max-w-[120px] py-0'>
           <span className='label-text text-accent'>{label}</span>
         </label>
         {editing ? (
           <input
             value={value}
-            className='input input-bordered'
+            className='input input-bordered w-full text-lg'
             onChange={(e) => {
               setValue(e.target.value);
             }}
           />
         ) : (
-          <div className='flex items-center h-10 w-3/5'>{value}</div>
+          <div className='flex items-center h-10 w-full'>{value}</div>
         )}
       </div>
       <div className='divider m-0' />
