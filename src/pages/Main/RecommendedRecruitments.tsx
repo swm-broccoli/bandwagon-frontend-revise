77e486467f0f27803dda5fe03203c2345edc9688
witch-factory <soakdma37@gmail.com>
2022-09-03 17:33:48 +0900

    구인구직글 배치 변경

--- src/pages/Main/RecommendedRecruitments.tsx
+++ src/pages/Main/RecommendedRecruitments.tsx
@@ -17,8 +17,13 @@ export const tempRecommendedRecruitments: RecommendedRecruitmentItemType[] = [
   },
   {
     image: 'https://picsum.photos/200',
-    region: '서울',
+    region: '경기',
     title: '일산 백석 직밴 건반 모십니다.',
+  },
+  {
+    image: 'https://picsum.photos/200',
+    region: '경기',
+    title: '경기 구리시 밴드 베이스 모십니다.',
   },
 ];
 
@@ -47,17 +52,19 @@ export function RecommendedRecruitments({
   recruitments: RecommendedRecruitmentItemType[];
 }) {
   return (
-    <section className='bg-[#f4f9f9] h-72 grid grid-cols-6 pt-6'>
-      <div className='col-start-2'>
+    <section className='bg-[#f4f9f9] h-72 flex flex-col md:flex-row self-center pt-6'>
+      <div className='col-start-2 place-self-center md:place-self-auto'>
         <h1 className='text-2xl font-bold'>추천 구인·구직글</h1>
         <h2 className='text-neutral'>R E C O M M E N D</h2>
       </div>
-      {recruitments.map((recruitment) => (
-        <RecommendedRecruitmentItem
-          key={recruitment.title}
-          recruitment={recruitment}
-        />
-      ))}
+      <div className='flex flex-row'>
+        {recruitments.map((recruitment) => (
+          <RecommendedRecruitmentItem
+            key={recruitment.title}
+            recruitment={recruitment}
+          />
+        ))}
+      </div>
     </section>
   );
 }
