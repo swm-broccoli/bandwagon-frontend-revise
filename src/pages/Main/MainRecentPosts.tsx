0074660b67dffb06377ef3594aa73dcac0f65303
witch-factory <soakdma37@gmail.com>
2022-09-03 22:46:15 +0900

    최근 인기글 디자인 어느 정도 완료

--- src/pages/Main/MainRecentPosts.tsx
+++ src/pages/Main/MainRecentPosts.tsx
@@ -84,12 +84,12 @@ export function MainRecentPosts({
   recentPosts: RecentPostItemType[];
 }) {
   return (
-    <section className='w-full flex justify-center items-center my-10 mx-8'>
-      <div className='w-full max-w-[1280px] flex flex-col items-center'>
+    <section className='w-full grid grid-cols-6 justify-center items-center py-10 md:mx-8'>
+      <div className='col-span-full lg:col-start-2 lg:col-end-6 flex flex-col items-center'>
         <h1 className='text-2xl font-bold'>최근 인기글</h1>
         <h2 className='text-neutral tracking-[0.2rem] mb-5'>P O P U L A R</h2>
-        <div className='w-full flex flex-row justify-between overflow-x-auto'>
-          {recentPosts.slice(0, 5).map((recentPost, index) => (
+        <div className='w-full max-w-[1280px] flex flex-row justify-between overflow-x-auto'>
+          {recentPosts.slice(0, 4).map((recentPost, index) => (
             <MainRecentPostItem key={index} recentPost={recentPost} />
           ))}
         </div>
