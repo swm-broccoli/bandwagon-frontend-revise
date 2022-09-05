06c52405127a9e4b7bacbf6ee7de9ee5d26cdd42
nabivi <nerdring54@gmail.com>
2022-09-05 21:37:22 +0900

    feat: 게시글 클릭시 링크 추가

--- src/pages/MyPost/index.tsx
+++ src/pages/MyPost/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import RecruitPostAPI from '../../apis/RecruitPostAPI';
 import ArticleCard from '../../components/ArticleCard';
 import MyPageTemplate from '../../components/MyPageTemplate';
@@ -56,14 +57,18 @@ function MyPostPage () {
         <div className='flex flex-col'>
           <Tab label='구직' />
           {userPost ?
-            <ArticleCard postInfo={userPost} /> :
+            <Link to={'/recruit/' + userPost.id.toString()}>
+              <ArticleCard postInfo={userPost} />
+            </Link> :
             <p className='w-fit text-lg text-[#464646] my-8 justify-self-center'>글이 존재하지 않습니다!</p>}
         </div>
         <div className='flex flex-col'>
           <Tab label='구인' />
           {bandPost ?
           bandPost.map((post, index) => 
-            <ArticleCard key={index} postInfo={post} />) :
+            <Link to={'/recruit/' + post.id.toString()}>
+              <ArticleCard key={index} postInfo={post} />
+            </Link>) :
             <p className='w-fit text-lg text-[#464646] my-8 justify-self-center'>글이 존재하지 않습니다!</p>
           }
         </div>
