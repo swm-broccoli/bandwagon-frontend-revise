31e19b0bc0793025052d9d4d6c8e7e0742c12527
witch-factory <soakdma37@gmail.com>
2022-09-20 15:42:17 +0900

    추천 구인 구직글 가져오는 api 추가

--- src/apis/MainPageAPI.ts
+++ src/apis/MainPageAPI.ts
@@ -4,11 +4,15 @@ const baseURL =
   'http://bandwagon-vpc-alb-private-dev-830505980.ap-northeast-2.elb.amazonaws.com/';
 
 interface MainPageApiType {
+  getRecommendedRecruits: () => Promise<AxiosResponse>;
   getPopularPosts: () => Promise<AxiosResponse>;
   getTodayPortfolios: () => Promise<AxiosResponse>;
 }
 
 const MainPageAPI: MainPageApiType = {
+  getRecommendedRecruits: () => {
+    return axios.get(baseURL + 'api/recommendations');
+  },
   getPopularPosts: () => axios.get(baseURL + 'api/post/popular'),
   getTodayPortfolios: () => axios.get(baseURL + 'api/random'),
 };
