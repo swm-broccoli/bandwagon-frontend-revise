ef350773101ae9e9cc0b222a654221b45797345a
witch-factory <soakdma37@gmail.com>
2022-10-14 22:09:13 +0900

    termagreebox 파일로 분리

--- src/pages/SignUp/styles.tsx
+++ src/pages/SignUp/styles.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TextInput from '../../components/TextInput';
-import CheckBox from '../../components/CheckBox';
+
 import DateInput from '../../components/DateInput';
 import SelectionInput from '../../components/SelectionInput';
 import AuthAPI from '../../apis/AuthAPI';
@@ -8,94 +8,11 @@ import { useNavigate } from 'react-router-dom';
 import { SignUpUserInputType } from '../../types/types';
 import { validateSignUpForm } from './validateSignUpForm';
 import SendbirdChat from '@sendbird/chat';
-
-interface CheckBoxItemType {
-  text: string;
-  checked: boolean;
-}
+import TermAgreeBox from './TermAgreeBox';
 
 function getTodayDate() {
   const today = new Date();
   return today.toISOString().split('T')[0];
-}
-
-function TermAgreeBox({
-  label,
-  setAgreed,
-}: {
-  label: string;
-  setAgreed: (newAgreed: boolean) => void;
-}) {
-  //약관 동의 박스
-  const [checkedList, setCheckedList] = useState<CheckBoxItemType[]>([
-    {
-      text: '만 14세 이상입니다.',
-      checked: false,
-    },
-    {
-      text: '서비스 이용약관에 동의합니다.',
-      checked: false,
-    },
-    {
-      text: '개인정보 수집 및 이용에 동의합니다.',
-      checked: false,
-    },
-    {
-      text: '위치정보 이용약관에 동의합니다.',
-      checked: false,
-    },
-  ]);
-
-  useEffect(() => {
-    setAgreed(checkedList.every((checked) => checked.checked === true));
-  }, [checkedList]);
-
-  return (
-    <div className='flex flex-col mt-5 w-60 md:w-80'>
-      <label className='text-accent mb-2 text-sm'>{label}</label>
-      <div className='border border-base-200 rounded-lg px-3 md:px-5'>
-        <div className='mt-2'>
-          <CheckBox
-            text='약관 전체 동의'
-            checked={checkedList.every((checked) => checked.checked === true)}
-            onClick={() => {
-              if (checkedList.every((checked) => checked.checked === true)) {
-                //만약 모두 true
-                setCheckedList(
-                  checkedList.map((checked) => ({
-                    ...checked,
-                    checked: false,
-                  })),
-                );
-              } else {
-                setCheckedList(
-                  checkedList.map((checked) => ({
-                    ...checked,
-                    checked: true,
-                  })),
-                );
-              }
-            }}
-          />
-        </div>
-        <div className='divider m-0' />
-        {checkedList.map((checked, index) => (
-          <CheckBox
-            key={index}
-            text={checked.text}
-            checked={checked.checked}
-            onClick={() => {
-              setCheckedList(
-                checkedList.map((item, i) =>
-                  i === index ? { ...item, checked: !item.checked } : item,
-                ),
-              );
-            }}
-          />
-        ))}
-      </div>
-    </div>
-  );
 }
 
 function SignUpForm() {
