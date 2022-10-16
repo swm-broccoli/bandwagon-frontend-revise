b78e4323655d20dc8a2c9802987294eca32cb6bf
witch-factory <soakdma37@gmail.com>
2022-10-16 16:13:23 +0900

    포트폴리오 정보 불러오는 api

--- src/pages/Portfolio/Display/index.tsx
+++ src/pages/Portfolio/Display/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import PortfolioAPI from '../../../apis/PortfolioAPI';
 
 function UserPortfolioDisplay() {
   const { userEmail } = useParams();
+
+  useEffect(() => {
+    console.log(userEmail);
+    PortfolioAPI.getUserPortfolioInfo(userEmail || '').then((res) => {
+      console.log(res);
+    });
+  }, []);
   return <div>{userEmail} 포트폴리오 페이지입니다.</div>;
 }
 
