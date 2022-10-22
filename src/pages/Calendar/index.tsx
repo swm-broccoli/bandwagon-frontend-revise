80b205b03421ccdc361474281c3eece2e57c5706
witch-factory <soakdma37@gmail.com>
2022-10-22 16:48:37 +0900

    달력 날짜 렌더링

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,6 +1,43 @@
 import { useState } from 'react';
 import { MdArrowBack, MdArrowForward } from 'react-icons/md';
 
+const Weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function prevMonthDates(date: Date) {
+  const prevDate = new Date(date.getFullYear(), date.getMonth(), 0);
+  const prevLastDate = prevDate.getDate();
+  const prevLastDay = prevDate.getDay();
+
+  const prevDates = [];
+  for (let i = prevLastDay + 1; i > 0; i--) {
+    prevDates.push(prevLastDate - i + 1);
+  }
+  return prevDates;
+}
+
+function nextMonthDates(date: Date) {
+  const nextDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+  const nextLastDay = nextDate.getDay();
+
+  const nextDates = [];
+  for (let i = 1; i <= 7 - nextLastDay; i++) {
+    nextDates.push(i);
+  }
+  return nextDates;
+}
+
+function currentMonthDates(date: Date) {
+  const currentMonth = date.getMonth();
+  const currentYear = date.getFullYear();
+  const currentLastDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  const dates = [];
+  for (let i = 1; i <= currentLastDate; i++) {
+    dates.push(i);
+  }
+  return dates;
+}
+
 function Calendar() {
   const today = new Date();
   const [currentDate, setCurrentDate] = useState(today);
@@ -27,6 +64,22 @@ function Calendar() {
         <button onClick={handleNextMonth} className='btn btn-xs btn-primary'>
           <MdArrowForward />
         </button>
+      </div>
+      <div className='grid grid-cols-7'>
+        {Weekdays.map((weekday) => (
+          <div>{weekday}</div>
+        ))}
+      </div>
+      <div className='grid grid-cols-7'>
+        {prevMonthDates(currentDate).map((date) => (
+          <div>{date}</div>
+        ))}
+        {currentMonthDates(currentDate).map((date) => (
+          <div>{date}</div>
+        ))}
+        {nextMonthDates(currentDate).map((date) => (
+          <div>{date}</div>
+        ))}
       </div>
     </section>
   );
