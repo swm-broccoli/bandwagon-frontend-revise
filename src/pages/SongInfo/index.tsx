cbcddd74617b1a5bad8a9fe0a9d45ee0d130cf45
nabivi <nerdring54@gmail.com>
2022-10-27 22:23:54 +0900

    feat: 노래 정보, 가사 정보 디자인 추가

--- src/pages/SongInfo/index.tsx
+++ src/pages/SongInfo/index.tsx
@@ -3,31 +3,74 @@ import GlobalFooter from '../../components/Footer';
 import GlobalNavBar from '../../components/NavBar';
 import ExamplePic from '../../assets/examplepic.png';
 
+const sessionList = ['기타', '베이스', '드럼', '키보드'];
+const siteInfoList = [{
+  site: '사이트 1',
+  sheet: [true, true, false, false]
+  },
+  {
+    site: '사이트 2',
+    sheet: [true, true, true, true]
+  },
+  {
+    site: '사이트 3',
+    sheet: [false, false, false, false]
+  },
+  {
+    site: '사이트 4',
+    sheet: [false, false, true, true]
+  }];
+
+function DivisionLine () {
+  return (
+    <div className='max-w-2xl w-10/12 bg-base-200 h-px' />
+  )
+}
+
+function SongBaiscInfo (props: {title: string, artist: string}) {
+  return (
+    <div className='flex flex-col gap-2 mb-8 items-center'>
+      <img src={ExamplePic} className='w-36 h-36 object-cover rounded-xl'/>
+      <h2 className='mt-2 text-xl text-accent'>{props.title}</h2>
+      <h4 className='text-lg text-neutral'>{props.artist}</h4>
+    </div>
+  )
+}
+
+function LyricsInfo (props: {lyrics: string}) {
+  return (
+    <div className="collapse collapse-arrow w-10/12 max-w-2xl">
+      <input type="checkbox" /> 
+        <div className="collapse-title text-base text-accent">
+          가사
+        </div>
+      <div className="collapse-content"> 
+        <p className='border border-base-200 rounded-xl p-4'>{props.lyrics}</p>
+      </div>
+    </div>
+  )
+}
+
 function SongInfoPage () {
   return (
     <>
       <GlobalNavBar />
-      <div className='flex flex-col my-4 gap-2 items-center'>
-        <img src={ExamplePic} className='w-36 h-36 object-cover rounded-xl'/>
-        <h2>노래 제목</h2>
-        <h4>가수</h4>
-        <div className='flex flex-col'>
-          <h2>가사</h2>
-          <p>가나다라마바사아자차카타파하</p>
-        </div>
-        <div className='grid grid-cols-6 grid-rows-4 gap-4'>
+      <div className='flex flex-col my-10 gap-4 items-center'>
+        <SongBaiscInfo title='노래 제목' artist='가수' />
+        <DivisionLine />
+        <LyricsInfo lyrics='가나다라마바사' />
+        <DivisionLine />
+        <div className='grid grid-cols-5 grid-rows-4 gap-4'>
           <h2>악보 정보</h2>
-          <h2 className='col-start-2'>보컬</h2>
-          <h2 className='col-start-3'>기타</h2>
-          <h2 className='col-start-4'>베이스</h2>
-          <h2 className='col-start-5'>피아노</h2>
-          <h2 className='col-start-6'>드럼</h2>
-          <h2 className='row-start-2'>사이트1</h2>
-          <p className='row-start-2 col-start-2'>O</p>
-          <p className='row-start-2 col-start-3'>X</p>
-          <p className='row-start-2 col-start-4'>X</p>
-          <p className='row-start-2 col-start-5'>O</p>
-          <p className='row-start-2 col-start-6'>O</p>
+          {sessionList.map((session, index) =>
+          <h2 className={'col-start-' + (index + 2).toString()}>{session}</h2>)}
+          {siteInfoList.map((site, index) =>
+            <h2 className={'row-start-' + (index + 2).toString()}>{site.site}</h2>)}
+          {siteInfoList.map((siteInfo, index) =>
+            siteInfo.sheet.map((sheet, index2) =>
+              <p className={'row-start-' + (index + 2).toString() + 'col-start-' + (index2 + 2).toString()}>
+                {sheet ? 'O' : 'X'}
+              </p>))}
         </div>
         <div className='flex flex-col'>
           <h2>이 곡을 연주한 밴드들</h2>
