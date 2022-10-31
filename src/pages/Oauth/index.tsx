77d3f1ac132089fdc4616be1792634356cb68361
witch-factory <soakdma37@gmail.com>
2022-10-31 11:56:17 +0900

    oauth 페이지 리다이렉션

--- src/pages/Oauth/index.tsx
+++ src/pages/Oauth/index.tsx
@@ -1,12 +1,26 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useLoginStore } from '../../stores/LoginStore';
 
 function OauthPage() {
-  const queryString = useParams();
+  const { email, accessToken, refreshToken } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const logIn = useLoginStore((state) => state.logIn);
 
   useEffect(() => {
-    console.log(queryString);
-  }, []);
+    console.log(email, accessToken, refreshToken);
+    sessionStorage.clear();
+    sessionStorage.setItem('accessToken', accessToken || '');
+    sessionStorage.setItem('refreshToken', refreshToken || '');
+    sessionStorage.setItem('userID', email || '');
+    // 소셜 로그인인지 확인하는 변수
+    sessionStorage.setItem('isSocial', 'true');
+    logIn(email || '');
+    alert('로그인 성공');
+    navigate('/');
+  }, [location]);
 
   return <div>Oauth 페이지</div>;
 }
