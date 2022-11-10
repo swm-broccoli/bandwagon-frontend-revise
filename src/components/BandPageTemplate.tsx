02681ea2cacbe8998a29b1bc7695636bbc67146d
witch-factory <soakdma37@gmail.com>
2022-11-10 19:39:42 +0900

    로그인해야 밴드 정보 볼 수 있도록 수정

--- src/components/BandPageTemplate.tsx
+++ src/components/BandPageTemplate.tsx
@@ -13,6 +13,8 @@ import bandPortfolioIcon from '../assets/bandPageIcons/bandPortfolioIcon.png';
 import bandPortfolioIconActive from '../assets/bandPageIcons/bandPortfolioIconActive.png';
 import bandApplyIcon from '../assets/bandPageIcons/bandApplyIcon.png';
 import bandApplyIconActive from '../assets/bandPageIcons/bandApplyIconActive.png';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const bandPageMenuItems = [
   {
@@ -48,6 +50,17 @@ const bandPageMenuItems = [
 ];
 
 function BandPageTemplate({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentUser = sessionStorage.getItem('userID');
+    console.log(currentUser);
+    if (!currentUser) {
+      alert('로그인을 해야 이용할 수 있습니다.');
+      navigate('/login');
+    }
+  }, []);
+
   return (
     <div>
       <GlobalNavBar />
